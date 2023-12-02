63d7346bf90f01214296a24b0484e19b3d26dda1
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2023-12-02 20:40:16 +0700

    added latin parser

--- public/script/converter.js
+++ public/script/converter.js
@@ -2,40 +2,40 @@
 let sundanese = {
     aksara: {
         swara: {
-            'a': '&#x1b83',
-            'i': '&#x1b84',
-            'u': '&#x1b85',
-            'é': '&#x1b86',
-            'o': '&#x1b87',
-            'e': '&#x1b88',
-            'eu': '&#x1b89',
+            'a': '\u1b83',
+            'i': '\u1b84',
+            'u': '\u1b85',
+            'é': '\u1b86',
+            'o': '\u1b87',
+            'e': '\u1b88',
+            'eu': '\u1b89',
         },
         ngalagena: {
-            'k': '&#x1b8a',
-            'q': '&#x1b8b',
-            'g': '&#x1b8c',
-            'ng': '&#x1b8d',
-            'c': '&#x1b8e',
-            'j': '&#x1b8f',
-            'z': '&#x1b90',
-            'ny': '&#x1b91',
-            't': '&#x1b92',
-            'd': '&#x1b93',
-            'n': '&#x1b94',
-            'p': '&#x1b95',
-            'f': '&#x1b96',
-            'v': '&#x1b97',
-            'b': '&#x1b98',
-            'm': '&#x1b99',
-            'y': '&#x1b9a',
-            'r': '&#x1b9b',
-            'l': '&#x1b9c',
-            'w': '&#x1b9d',
-            's': '&#x1b9e',
-            'x': '&#x1b9f',
-            'h': '&#x1ba0',
-            'kh': '&#x1bae',
-            'sy': '&#x1baf',
+            'k': '\u1b8a',
+            'q': '\u1b8b',
+            'g': '\u1b8c',
+            'ng': '\u1b8d',
+            'c': '\u1b8e',
+            'j': '\u1b8f',
+            'z': '\u1b90',
+            'ny': '\u1b91',
+            't': '\u1b92',
+            'd': '\u1b93',
+            'n': '\u1b94',
+            'p': '\u1b95',
+            'f': '\u1b96',
+            'v': '\u1b97',
+            'b': '\u1b98',
+            'm': '\u1b99',
+            'y': '\u1b9a',
+            'r': '\u1b9b',
+            'l': '\u1b9c',
+            'w': '\u1b9d',
+            's': '\u1b9e',
+            'x': '\u1b9f',
+            'h': '\u1ba0',
+            'kh': '\u1bae',
+            'sy': '\u1baf',
         }
     },
     rarangken: {
@@ -43,85 +43,85 @@ let sundanese = {
             'i': {
                 name: 'Panghulu',
                 desc: 'Mengubah vokal aksara <i>Ngalagena<i> dari /a/ menjadi /i/',
-                char: '&#x1ba4',
+                char: '\u1ba4',
             },
             'u': {
                 name: 'Panyuku',
                 desc: 'Mengubah vokal aksara <i>Ngalagena<i> dari /a/ menjadi /u/',
-                char: '&#x1ba5',
+                char: '\u1ba5',
             },
             'é': {
                 name: 'Panéléng',
                 desc: 'Mengubah vokal aksara <i>Ngalagena<i> dari /a/ menjadi /é/',
-                char: '&#x1ba6',
+                char: '\u1ba6',
             },
             'o': {
                 name: 'Panolong',
                 desc: 'Mengubah vokal aksara <i>Ngalagena<i> dari /a/ menjadi /o/',
-                char: '&#x1ba7',
+                char: '\u1ba7',
             },
             'e': {
                 name: 'Pamepet',
                 desc: 'Mengubah vokal aksara <i>Ngalagena<i> dari /a/ menjadi /e/',
-                char: '&#x1ba8',
+                char: '\u1ba8',
             },
             'eu': {
                 name: 'Paneuleung',
                 desc: 'Mengubah vokal aksara <i>Ngalagena<i> dari /a/ menjadi /eu/',
-                char: '&#x1ba9',
+                char: '\u1ba9',
             },
         },
         sisip: {
             'y': {
                 name: 'Pamingkal',
                 desc: 'Menambah konsonan /y/ di tengah suku kata',
-                char: '&#x1ba1',
+                char: '\u1ba1',
             },
             'r': {
                 name: 'Panyakra',
                 desc: 'Menambah konsonan /r/ di tengah suku kata',
-                char: '&#x1ba2',
+                char: '\u1ba2',
             },
             'l': {
                 name: 'Panyiku',
                 desc: 'Menambah konsonan /l/ di tengah suku kata',
-                char: '&#x1ba3',
+                char: '\u1ba3',
             },
         },
         akhir: {
             'ng': {
                 name: 'Panyecek',
                 desc: 'Menambah konsonan /ng/ di akhir suku kata',
-                char: '&#x1b80',
+                char: '\u1b80',
             },
             'r': {
                 name: 'Panglayar',
                 desc: 'Menambah konsonan /r/ di akhir suku kata',
-                char: '&#x1b81',
+                char: '\u1b81',
             },
             'h': {
                 name: 'Pangwisad',
                 desc: 'Menambah konsonan /h/ di akhir suku kata',
-                char: '&#x1b82',
+                char: '\u1b82',
             },
             '/': {
                 name: 'Pamaéh',
                 desc: 'Meniadakan vokal pada suku kata',
-                char: '&#x1baa',
+                char: '\u1baa',
             },
         },
     },
     angka: [
-        '&#x1bb0',
-        '&#x1bb1',
-        '&#x1bb2',
-        '&#x1bb3',
-        '&#x1bb4',
-        '&#x1bb5',
-        '&#x1bb6',
-        '&#x1bb7',
-        '&#x1bb8',
-        '&#x1bb9',
+        '\u1bb0',
+        '\u1bb1',
+        '\u1bb2',
+        '\u1bb3',
+        '\u1bb4',
+        '\u1bb5',
+        '\u1bb6',
+        '\u1bb7',
+        '\u1bb8',
+        '\u1bb9',
     ]
 };
 
@@ -131,8 +131,8 @@ let sundanese = {
  * every syllable in the text.
  */
 const parser = {
-    latin: /(([^aiueéo\s\d\W])([rly])?(eu|[aiueéo])?|(?<![b-df-hj-np-tv-z])(eu|[aiueéo])|(\d+)|(\W+))((?<=[aiueéo])([rh]|ng)(?![aiueéo]))?/gi,
-    sundanese: /([\u1b83-\u1b89]|[\u1b8a-\u1ba0\u1bae-\u1baf][\u1ba1-\u1ba3]?[\u1ba4-\u1ba9]?)/gi
+    latin: /((ny|ng|kh|sy|[^aiueéo\s\d\W])([rly])?(eu|[aiueéo])?|(?<![b-df-hj-np-tv-z])(eu|[aiueéo])|(\d+)|(\W+))((?<=[aiueéo])([rh]|ng)(?![aiueéo]))?/gi,
+    sundanese: /(([\u1b83-\u1b89]|[\u1b8a-\u1ba0\u1bae-\u1baf][\u1ba1-\u1ba3]?[\u1ba4-\u1ba9]?)([\u1b80-\u1b82]|(?<=[\u1b83-\u1ba0\u1bae-\u1baf])\u1baa)?|[\u1bb0-\u1bb9]+)/gi
 }
 
 function latinToSundanese(text) {
@@ -140,7 +140,7 @@ function latinToSundanese(text) {
     let result = '';
     let parsed = text.matchAll(parser.latin);
     let aParsed;
-    
+
     while (true) {
         aParsed = parsed.next();
         if (aParsed.done) break;
@@ -148,30 +148,30 @@ function latinToSundanese(text) {
         if (aParsed.value[0].match(/[a-zé]/i)) {
 
             if (aParsed.value[2]) {
-                result += sundanese.aksara.ngalagena[aParsed.value[2]] + ';';
+                result += sundanese.aksara.ngalagena[aParsed.value[2]];
 
                 if (aParsed.value[0].length === 1) {
-                    result += sundanese.rarangken.akhir['/'].char + ';';
+                    result += sundanese.rarangken.akhir['/'].char;
                     continue;
                 }
 
                 if (aParsed.value[3])
-                    result += sundanese.rarangken.sisip[aParsed.value[3]].char + ';';
+                    result += sundanese.rarangken.sisip[aParsed.value[3]].char;
 
                 if (aParsed.value[4])
                     if (aParsed.value[4] == 'a') null;
-                    else result += sundanese.rarangken.vokal[aParsed.value[4]].char + ';';
+                    else result += sundanese.rarangken.vokal[aParsed.value[4]].char;
 
-            } else result += sundanese.aksara.swara[aParsed.value[5]] + ';';
+            } else result += sundanese.aksara.swara[aParsed.value[5]];
 
             if (aParsed.value[9])
-                result += sundanese.rarangken.akhir[aParsed.value[9]].char + ';';
+                result += sundanese.rarangken.akhir[aParsed.value[9]].char;
 
         } else if (aParsed.value[0].match(/\d/)) {
             result += '|';
 
             for (digit of aParsed.value[0])
-                result += sundanese.angka[digit] + ';';
+                result += sundanese.angka[digit];
 
             result += '|';
         } else result += aParsed.value[0];
